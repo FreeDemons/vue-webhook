c9510741c765b8a5b16d5e0b7d882b11eae298ed
freedemons <646714507@qq.com>
2020-02-15 03:08:23 +0800

    add qixi.sh

---
+++ qixi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WORK_PATH='/usr/projects/qixi'
+cd $WORK_PATH
+echo "先清除旧代码"
+git reset --hard origin/mater 
+git clean -f
+echo "拉取新代码"
+git pull origin master
+
+echo "开始执行构建"
+docker build -t qixi:1.0 .
+echo "停止旧容器并删除旧容器"
+docker stop qixi-container
+docker rm qixi-container
+echo "启动新容器"
+docker container run -p 7777:7777 --name qixi-container -d qixi:2.0
